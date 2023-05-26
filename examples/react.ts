6e5db7fdbf6eba698484593a8e0985f4c1f3621d
Levan Kvirkvelia <lekvirkvelia@gmail.com>
2023-05-25 18:43:28 -0700

    Update react.ts

--- examples/react.ts
+++ examples/react.ts
@@ -1,4 +1,4 @@
-import { assistant, gen, gpt4, loop, system, user, wait } from "..";
+import { assistant, gen, gpt4, system } from "..";
 import { block } from "../src/actions/actions";
 
 async function main() {
@@ -12,9 +12,9 @@ async function main() {
       block(
         [
           user`
-          Can you please generate one option for how to accomplish this?
-          Please make the option very short, at most one line.
-        `,
+            Can you please generate one option for how to accomplish this?
+            Please make the option very short, at most one line.
+          `,
           assistant`${gen("option", { temperature: 1, maxTokens: 500, n: 5 })}`,
         ],
         { hidden: () => outputs.option?.length > 0 }
@@ -22,23 +22,23 @@ async function main() {
       block(
         [
           user`
-          Can you please comment on the pros and cons of each of the following options, and then pick the best option?
-          ---
-          ${({ outputs }) =>
-            outputs.option.map((o, i) => `Option ${i}: ${o}`).join("\n")}
-          ---
-          Please discuss each option very briefly (one line for pros, one for cons), and end by saying Best=X, where X is the best option.
-        `,
+            Can you please comment on the pros and cons of each of the following options, and then pick the best option?
+            ---
+            ${({ outputs }) =>
+              outputs.option.map((o, i) => `Option ${i}: ${o}`).join("\n")}
+            ---
+            Please discuss each option very briefly (one line for pros, one for cons), and end by saying Best=X, where X is the best option.
+          `,
           assistant`${gen("prosandcons", { temperature: 0, maxTokens: 500 })}`,
         ],
         { hidden: () => !!outputs.prosandcons }
       ),
       user`
-      Here is my plan:
-      ${({ outputs }) =>
-        outputs.option[+(outputs.prosandcons.match(/Best=(\d+)/)?.[1] || 0)]}
-      Please elaborate on this plan, and tell me how to best accomplish it.
-    `,
+        Here is my plan:
+        ${({ outputs }) =>
+          outputs.option[+(outputs.prosandcons.match(/Best=(\d+)/)?.[1] || 0)]}
+        Please elaborate on this plan, and tell me how to best accomplish it.
+      `,
       assistant`${gen("plan", { maxTokens: 500 })}`,
     ],
     { stream: true }
